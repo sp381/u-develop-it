ef0a961651f625a38bd13da82f56ee5321b0716d
sp381 <sarah.powell381@gmail.com>
2022-04-13 14:38:19 -0700

    updated code to match snapshot

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,12 @@
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
@@ -9,10 +15,3 @@ CREATE TABLE candidates (
   industry_connected BOOLEAN NOT NULL,
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
-
-CREATE TABLE parties (
-    id INTEGAR AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    description TEXT
-);
-
