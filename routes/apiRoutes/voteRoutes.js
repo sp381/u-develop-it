552039c30df3350d3dec43c26eb8a5945561da55
sp381 <sarah.powell381@gmail.com>
2022-04-14 17:13:16 -0700

    added a vote route

---
+++ routes/apiRoutes/voteRoutes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../utils/inputCheck');
+
+router.post('/vote', ({ body }, res) => {
+    // Data validation
+    const errors = inputCheck(body, 'voter_id', 'candidate_id');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+  
+    const sql = `INSERT INTO votes (voter_id, candidate_id) VALUES (?,?)`;
+    const params = [body.voter_id, body.candidate_id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body,
+        changes: result.affectedRows
+      });
+    });
+  });
+  
+  router.get('/votes')
+
+
+  module.exports = router;
